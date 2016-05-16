14a0a64c55f7e1a3687ce5b100fc54c26a77c1c4
xijiaxiang <2211508055@ujs.edu.cn>
2016-05-16 11:24:34 +0800

    add login filter

--- src/filter/AuthenFilter.java
+++ src/filter/AuthenFilter.java
@@ -1,7 +1,35 @@
 package filter;
 
+import bean.Consumer;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by xijiaxiang on 5/16/16.
  */
-public class AuthenFilter {
+public class AuthenFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        Consumer consumer = (Consumer) request.getSession().getAttribute("consumer");
+        if (consumer == null) {
+            response.sendRedirect("/userLogin.jsp");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
